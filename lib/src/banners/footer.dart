fb9a5c0db4db17fac1a591a130d096abf44f32c9
aguchi94 <agudemoreno2@gmail.com>
2026-06-09 19:48:59 -0300

    update

--- lib/src/banners/footer.dart
+++ lib/src/banners/footer.dart
@@ -20,7 +20,44 @@ class DesktopFooter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(width: double.infinity, height: 500, color: Colors.blue);
+    return Container(
+      width: double.infinity,
+      height: 500,
+      decoration: BoxDecoration(color: Colors.black),
+      child: Row(
+        children: [
+          //imagen
+          Expanded(child: Placeholder()),
+          //informacion de contacto COLUMN
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  "Contacto",
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Color(0xFF76d0cf),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Direccion : ",
+                  style: TextStyle(fontSize: 30, color: Color(0xFF76d0cf)),
+                ),
+                Text(
+                  "Tel : ",
+                  style: TextStyle(fontSize: 30, color: Color(0xFF76d0cf)),
+                ),
+                Text(
+                  "Redes",
+                  style: TextStyle(fontSize: 40, color: Color(0xFF76d0cf)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
