68c428a69b709a2c4760f9dd0a7f04f1cae0e7a6
aguchi94 <agudemoreno2@gmail.com>
2026-06-09 18:34:09 -0300

    update whatsapp icon

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:web_pruebas_plazadig/src/banners/footer.dart';
 import 'package:web_pruebas_plazadig/src/banners/portada.dart';
+import 'package:web_pruebas_plazadig/src/globals/whatsapp_button.dart';
 
 class HomePageScreenUi extends StatefulWidget {
   const HomePageScreenUi({super.key});
@@ -12,11 +13,16 @@ class HomePageScreenUi extends StatefulWidget {
 class _HomePageScreenUiState extends State<HomePageScreenUi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.black,
       body: Stack(
         children: [
           SingleChildScrollView(child: Column(children: [Portada(), Footer()])),
+          WhatsAppFloatingButton(
+            phoneNumber:
+                '5491112345678', // Reemplazar con el número real de WhatsApp
+            imagePath: 'assets/icons/wsp-logo-64px.webp',
+          ),
         ],
       ),
     );
